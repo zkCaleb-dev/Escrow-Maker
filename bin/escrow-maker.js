7aaf51f45c4ec63a85fc7dc876be1142b9275767
Caleb Loria Gomez <zkcaleb@Calebs-MacBook-Pro.local>
2025-06-05 22:47:26 -0600

    feat: Add the deploy-multi command

--- bin/escrow-maker.js
+++ bin/escrow-maker.js
@@ -3,14 +3,10 @@
 const { Command } = require('commander');
 const program = new Command();
 
-// 1) Importamos los subcomandos tipo Command:
-const deployCmd = require('../lib/commands/deploy');      // exporta directamente el Command('deploy')
-const configCmd = require('../lib/commands/config');      // exporta directamente el Command('config')
+const deploySingleCmd = require('../lib/commands/deploy-single');  
+const deployMultiCmd = require('../lib/commands/deploy-multi');  
+const configCmd = require('../lib/commands/config');
 
-// 2) Importamos el comando de saludo (init.js) que sí es una función
-const { saludar } = require('../lib/commands/init');
-
-// 3) Importamos el subcomando sign (exporta { signXdr: signCmd })
 const { signXdr: signCmd } = require('../lib/commands/sign');
 
 program
@@ -18,25 +14,11 @@ program
   .version('1.0.0')
   .description('CLI “escrow” para interactuar con Trustless Work');
 
-// 4) Registramos el subcomando `config`
-//    (lib/commands/config.js exporta un objeto Command con todos sus subcomandos)
 program.addCommand(configCmd);
 
-// 5) Registramos el subcomando `deploy`
-//    (lib/commands/deploy.js exporta un objeto Command('deploy') ya configurado)
-program.addCommand(deployCmd);
+program.addCommand(deploySingleCmd);
+program.addCommand(deployMultiCmd);
 
-// 6) Registramos el subcomando `sign`
-//    (lib/commands/sign.js exportó { signXdr: signCmd }, donde signCmd es el objeto Command)
 program.addCommand(signCmd);
 
-// 7) El comando “greet” sigue siendo una función manual, así que lo definimos con `.command().action()`
-program
-  .command('greet <nombre>')
-  .description('Saluda al nombre proporcionado.')
-  .action((nombre, opts) => {
-    saludar(nombre, opts);
-  });
-
-// 8) Finalmente parseamos los argumentos
 program.parse(process.argv);
